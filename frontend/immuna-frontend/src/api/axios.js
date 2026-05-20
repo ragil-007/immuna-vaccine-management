763753986d74817c162f11655362351a86ba8104
Ragil <ragilraghunath114@gmail.com>
2026-05-20 17:03:33 +0530

    connect frontend to production backend

--- frontend/immuna-frontend/src/api/axios.js
+++ frontend/immuna-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://immuna-vaccine-management.onrender.com/api"
 })
 
 // Attach JWT automatically
